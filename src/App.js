502cfeb78789807daf393ad68b91c8530a176ea0
brandodo <brandonong93@gmail.com>
2022-03-12 18:25:33 -0500

    added button for music

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Giphy from "./Components/Giphy";
 import bump from "./assets/sounds/bump.mp3";
 import fart from "./assets/sounds/fart.mp3";
 import wow from "./assets/sounds/wow.mp3";
+
 const POKE_API_URL = "https://pokeapi.co/api/v2/pokemon/";
 const GIPHY_API_URL = "https://api.giphy.com/v1/gifs/search";
 const GIPHY_API_KEY = "eFXa6sZe3b1BwD889es0gi4VBFlhUPAT";
@@ -63,8 +64,11 @@ class App extends React.Component {
     // check clicked answer and display gif
     const checkAnswer = (name) => {
       if (name === this.state.name) {
+
+        // play wow
         const wowObj = new Audio(wow);
         wowObj.play();
+        
         // right answer: get gif, show pokemon, and reset state values
         axios
           .get(`${GIPHY_API_URL}?q="happy pokemon"&api_key=${GIPHY_API_KEY}`)
@@ -91,8 +95,11 @@ class App extends React.Component {
 
         // wrong answer: get gif
       } else {
+        
+        // play fart sound
         const fartObj = new Audio(fart);
         fartObj.play();
+
         axios
           .get(`${GIPHY_API_URL}?q="sad pokemon"&api_key=${GIPHY_API_KEY}`)
           .then((res) => {
@@ -107,18 +114,27 @@ class App extends React.Component {
           });
       }
     };
-    // const playMusic = () => {
-    //   if (this.state.playAudio === false) {
-    //     this.setState({ playAudio: true });
-    //     const theme = new Audio(pokemonTheme);
-    //     theme.loop = true;
-    //     theme.play();
-    //   }
-    // };
+
+    const playMusic = () => {
+      let play = !this.state.playAudio;
+      this.setState({ playAudio: play });
+
+      const theme = new Audio(pokemonTheme);
+      theme.loop = true;
+      theme.volume = 0.10;
+
+      if (play === false) {
+        theme.pause();
+      } else {
+        theme.play();
+      }
+    };
+
     const sound = () => {
       const audioObj = new Audio(bump);
       audioObj.play();
     };
+
     const boxes = this.state.answerArray.map((name, index) => {
       return (
         <Input
@@ -132,10 +148,9 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <iframe src={pokemonTheme} allow="autoPlay" className="music"></iframe>
-        <audio autoplay loop>
-          <source src={pokemonTheme} type="audio/mp3" />
-        </audio>
+        <button className="music-button" onClick={() => playMusic()}>
+          Play Music
+        </button>
         <div className="background">
           <Pokemon image={this.state.image} show={this.state.showPokemon} />
         </div>
